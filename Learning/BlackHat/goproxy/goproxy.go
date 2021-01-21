666a2ee152021d6e90540e41d081c73422f0262a
oonray <alexanderbjornsrud@gmail.com>
2021-01-21 09:03:09 +0100

    .

--- Learning/BlackHat/goproxy/goproxy.go
+++ Learning/BlackHat/goproxy/goproxy.go
@@ -1,1 +1,3 @@
-package goproxy
+package main
+
+
---
+++ Learning/BlackHat/gospider/gospider.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"github.com/Willyham/gospider/spider"
+	"github.com/sirupsen/logrus"
+	"net/url"
+	"os"
+	"time"
+)
+
+func main(){
+	u := flag.String("url","","The url To Spider")
+	flag.Parse()
+
+	uri,_ := url.Parse(*u)
+	spdr := spider.New(
+		spider.WithRoot(uri),
+		spider.WithConcurrency(5),
+		spider.WithTimeout(time.Second*2),
+		)
+
+	err := spdr.Run()
+	if(err != nil){
+		logrus.Errorf("Spider Failed: %s",err)
+	}
+
+	err = spdr.Report(os.Stdout)
+	if(err != nil){
+		logrus.Errorf("Spider Failed: %s",err)
+	}
+
+	return
+}
