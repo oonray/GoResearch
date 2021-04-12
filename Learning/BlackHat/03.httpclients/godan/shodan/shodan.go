a776ba534bdfbb78f5fcabd8c280f412861c70b0
oonray <alexanderbjornsrud@gmail.com>
2021-04-12 12:59:01 +0200

    .

--- Learning/BlackHat/03.httpclients/godan/shodan/shodan.go
+++ Learning/BlackHat/03.httpclients/godan/shodan/shodan.go
@@ -2,12 +2,9 @@ package shodan
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/shadowscatcher/shodan"
 	"github.com/sirupsen/logrus"
 )
 
@@ -99,34 +96,5 @@ func (c *Client)HostSearch(query string) (*HostSearch,error){
 		return nil, err
 	}
 	return &ret, err	
-}
-
-func main(){
-	search := flag.String("s","","The Search Term")
-	flag.Parse()
-
-	if(*search == ""){
-		logrus.Errorf("Search term is manditory")
-		flag.PrintDefaults()	
-	}
-	
-	apiKey := os.Getenv("SHODAN_API_KEY")
-	s := shodan.New(apiKey)
-
-	info, err := s.ApiInfo()
-	if(err != nil){
-		logrus.Errorf("Could not get API info %s",err)
-	}
-
-	logrus.Infof("Query Credits: %d\nScan Credits: %d\n\n",info.Query_Credits,info.Scan_Credits)
-
-	hostSearch, err := s.HostSearch(*search)
-	if err != nil {
-		logrus.Errorf("Problem making the search %s",err)
-	}
-
-	for _,host := range hostSearch.Matches{
-		logrus.Infof("%18s%8d\n",host.IPString,host.Port)
-	}
 }
 
